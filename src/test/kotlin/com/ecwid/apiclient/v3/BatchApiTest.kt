15a24b65462294b8dad801989fd9ea5a8df5b99f
Anton Piontkovskiy <jant@ecwid.com>
2019-07-30 10:49:25 +0400

    Improved batch awaiting support for tests

--- src/test/kotlin/com/ecwid/apiclient/v3/BatchApiTest.kt
+++ src/test/kotlin/com/ecwid/apiclient/v3/BatchApiTest.kt
@@ -2,6 +2,7 @@ package com.ecwid.apiclient.v3
 
 import com.ecwid.apiclient.v3.dto.batch.request.CreateBatchRequest
 import com.ecwid.apiclient.v3.dto.batch.request.GetTypedBatchRequest
+import com.ecwid.apiclient.v3.dto.batch.result.BatchStatus
 import com.ecwid.apiclient.v3.dto.product.request.ProductCreateRequest
 import com.ecwid.apiclient.v3.dto.product.request.ProductsSearchRequest
 import com.ecwid.apiclient.v3.dto.product.request.UpdatedProduct
@@ -12,7 +13,6 @@ import com.ecwid.apiclient.v3.util.randomAlphanumeric
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.util.concurrent.TimeUnit
 
 class BatchApiTest : BaseEntityTest() {
 
@@ -53,8 +53,8 @@ class BatchApiTest : BaseEntityTest() {
 
 		val createResponses = createProductBatchResult.responses
 		require(createResponses != null)
-		val productCreateResult = createResponses.first().toTypedResponse(ProductCreateResult::class.java)
-		when (productCreateResult) {
+		when (val productCreateResult =
+				createResponses.first().toTypedResponse(ProductCreateResult::class.java)) {
 
 			is TypedBatchResponse.Ok -> {
 				Assertions.assertTrue(productCreateResult.value.id > 0)
@@ -74,7 +74,7 @@ class BatchApiTest : BaseEntityTest() {
 		)
 
 		// No way to predict when this query will be executed, let's simply wait
-		TimeUnit.SECONDS.sleep(5)
+		awaitBatchExecution(searchProductBatch.ticket)
 
 		val searchProductBatchResult = apiClient.getTypedBatch(
 				GetTypedBatchRequest(
@@ -84,8 +84,8 @@ class BatchApiTest : BaseEntityTest() {
 
 		val searchResponses = searchProductBatchResult.responses
 		require(searchResponses != null)
-		val productsSearchResult = searchResponses.first().toTypedResponse(ProductsSearchResult::class.java)
-		when (productsSearchResult) {
+		when (val productsSearchResult =
+				searchResponses.first().toTypedResponse(ProductsSearchResult::class.java)) {
 
 			is TypedBatchResponse.Ok -> {
 				Assertions.assertTrue(productsSearchResult.value.count > 0)
@@ -94,5 +94,20 @@ class BatchApiTest : BaseEntityTest() {
 			is TypedBatchResponse.ParseError -> Assertions.fail("Parse error is unexpected")
 			is TypedBatchResponse.NotExecuted -> Assertions.fail("Not executed error is not expected")
 		}
+	}
+
+	private fun awaitBatchExecution(ticket: String) {
+		var tries = 0
+
+		do {
+			val batchResult = apiClient.getTypedBatch(GetTypedBatchRequest(ticket = ticket))
+			if (batchResult.status == BatchStatus.COMPLETED) {
+				return
+			}
+			tries++
+			Thread.sleep(500L * tries)
+		} while (tries < 10)
+
+		return Assertions.fail("Failed to await for batch completion")
 	}
 }
