32f28ca7a39bf825cf47bea45335fa1ec311c828
Anton Piontkovskiy <jant@ecwid.com>
2019-08-01 14:26:21 +0400

    Implemented category translations support

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/UpdatedCategory.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/UpdatedCategory.kt
@@ -4,7 +4,9 @@ data class UpdatedCategory(
 		val parentId: Int? = null,
 		val orderBy: Int? = null,
 		val name: String? = null,
+		val nameTranslated: Map<String, String>? = null,
 		val description: String? = null,
+		val descriptionTranslated: Map<String, String>? = null,
 		val enabled: Boolean? = null,
 		val productIds: List<Int>? = null
 )
