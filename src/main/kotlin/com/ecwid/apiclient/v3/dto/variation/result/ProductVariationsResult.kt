1c72b733fcb8c29a14ec9ddb17e5d7174cbb0643
Igor Kurguzov <igor@ecwid.com>
2020-02-20 10:26:06 +0400

    corrected the response contents for getAllProductVariations

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/variation/result/ProductVariationsResult.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/variation/result/ProductVariationsResult.kt
@@ -1,9 +1,5 @@
 package com.ecwid.apiclient.v3.dto.variation.result
 
 data class ProductVariationsResult(
-		val items: List<FetchedVariation> = listOf(),
-		val count: Int = 0,
-		val total: Int = 0,
-		val limit: Int = 0,
-		val offset: Int = 0
+		val variations: List<FetchedVariation> = listOf()
 )
