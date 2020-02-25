e347bf1ffb6ee29ff5ed9e875988a51455611575
Andrey Kukin <skip@ecwid.com>
2020-02-25 18:21:35 +0400

    small fixes for carts methods

--- src/main/kotlin/com/ecwid/apiclient/v3/impl/CartsApiClientImpl.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/impl/CartsApiClientImpl.kt
@@ -7,21 +7,21 @@ import com.ecwid.apiclient.v3.dto.cart.result.*
 internal data class CartsApiClientImpl(
         private val apiClientHelper: ApiClientHelper
 ) : CartsApiClient {
-    override fun searchAbandonedCart(request: CartsSearchRequest) = apiClientHelper.makeRequest<CartsSearchResult>(request)
+    override fun searchCarts(request: CartsSearchRequest) = apiClientHelper.makeRequest<CartsSearchResult>(request)
 
-    override fun searchAbandonedCartsAsSequence(request: CartsSearchRequest): List<FetchedCart> {
+    override fun searchCartsAsSequence(request: CartsSearchRequest): List<FetchedCart> {
         var offsetRequest = request
         val totalList: MutableList<FetchedCart> = mutableListOf()
         do {
-            val searchResult = searchAbandonedCart(offsetRequest)
+            val searchResult = searchCarts(offsetRequest)
             totalList.addAll(searchResult.items)
             offsetRequest = offsetRequest.copy(offset = offsetRequest.offset + searchResult.count)
         } while (searchResult.count >= searchResult.limit)
         return totalList
     }
 
-    override fun getAbandonedCart(request: CartDetailsRequest) = apiClientHelper.makeRequest<FetchedCart>(request)
-    override fun updateAbandonedCart(request: CartUpdateRequest) = apiClientHelper.makeRequest<CartUpdateResult>(request)
+    override fun getCartDetails(request: CartDetailsRequest) = apiClientHelper.makeRequest<FetchedCart>(request)
+    override fun updateCart(request: CartUpdateRequest) = apiClientHelper.makeRequest<CartUpdateResult>(request)
     override fun calculateOrderDetails(request: CalculateOrderDetailsRequest) = apiClientHelper.makeRequest<CalculateOrderDetailsResult>(request)
-    override fun convertAbandonedCartToOrder(request: ConvertCartToOrderRequest) = apiClientHelper.makeRequest<ConvertCartToOrderResult>(request)
+    override fun convertCartToOrder(request: ConvertCartToOrderRequest) = apiClientHelper.makeRequest<ConvertCartToOrderResult>(request)
 }
