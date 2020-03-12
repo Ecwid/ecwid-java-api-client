029193fa04fa92bfafd90e6c41b6d73e89817544
Igor Kurguzov <igor@ecwid.com>
2020-03-12 15:25:36 +0400

    added languages and shipping sections to FetchedStoreProfile

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/FetchedStoreProfile.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/FetchedStoreProfile.kt
@@ -6,7 +6,9 @@ data class FetchedStoreProfile(
         var settings: Settings? = null,
         var mailNotifications: MailNotifications? = null,
         var company: Company? = null,
-        var formatsAndUnits: FormatsAndUnits? = null
+        var formatsAndUnits: FormatsAndUnits? = null,
+        var languages: Languages? = null,
+        var shipping: Shipping? = null
 ) {
     data class GeneralInfo(
             var storeId: Int = 0,
@@ -125,4 +127,139 @@ data class FetchedStoreProfile(
     enum class WeightUnit {
         CARAT, GRAM, OUNCE, POUND, KILOGRAM
     }
+
+    data class Languages(
+            var enabledLanguages: List<String>? = null,
+            var facebookPreferredLocale: String? = null,
+            var defaultLanguage: String? = null
+    )
+
+    data class Shipping(
+            var handlingFee: HandlingFee? = null,
+            var shippingOrigin: ShippingOrigin? = null,
+            var shippingOptions: List<ShippingOption>? = null
+    )
+
+    data class HandlingFee(
+            var name: String? = null,
+            var value: Double? = null,
+            var description: String? = null
+    )
+
+    data class ShippingOrigin(
+            var companyName: String? = null,
+            var email: String? = null,
+            var street: String? = null,
+            var city: String? = null,
+            var countryCode: String? = null,
+            var postalCode: String? = null,
+            var stateOrProvinceCode: String? = null,
+            var phone: String? = null
+    )
+
+    data class ShippingOption(
+            var id: String? = null,
+            var title: String? = null,
+            var enabled: Boolean? = null,
+            var orderBy: Int? = null,
+            var fulfillmentType: FulfillmentType? = null,
+            var destinationZone: Zone? = null,
+            var deliveryTimeDays: String? = null,
+            var description: String? = null,
+            var carrier: String? = null,
+            var carrierMethods: List<CarrierMethod>? = null,
+            var carrierSettings: CarrierSettings? = null,
+            var ratesCalculationType: RatesCalculationType? = null,
+            var shippingCostMarkup: Double? = null,
+            var flatRate: FlatRate? = null,
+            var ratesTable: TableRatesDetails? = null,
+            var appClientId: String? = null,
+            var pickupInstruction: String? = null,
+            var scheduledPickup: Boolean? = null,
+            var pickupPreparationTimeHours: Int? = null,
+            var pickupBusinessHours: String? = null
+    )
+
+    enum class FulfillmentType {
+        pickup, shipping
+    }
+
+    data class Zone(
+        var id: String? = null,
+        var name: String? = null,
+        var countryCodes: List<String>? = null,
+        var stateOrProvinceCodes: List<String>? = null,
+        var postCodes: List<String>? = null
+    )
+
+    data class CarrierMethod(
+            var id: String? = null,
+            var name: String? = null,
+            var enabled: Boolean? = null,
+            var orderBy: Int? = null
+    )
+
+    data class CarrierSettings(
+            var defaultCarrierAccountEnabled: Boolean? = null,
+            var defaultPostageDimensions: DefaultPostageDimensions? = null
+    )
+
+    data class DefaultPostageDimensions(
+            var length: Int? = null,
+            var width: Int? = null,
+            var height: Int? = null
+    )
+
+    enum class RatesCalculationType {
+        carrier_calculated, table, flat, app;
+
+        override fun toString(): String {
+            return super.toString().replace("_", "-")
+        }
+    }
+
+    fun RatesCalculationType.valueOf(value: String?): RatesCalculationType? {
+        return RatesCalculationType.values().firstOrNull() { it.toString() == value }
+    }
+
+    data class FlatRate(
+            var rateType: RateType? = null,
+            var rate: Double? = null
+    ) {
+        enum class RateType {
+            ABSOLUTE, PERCENT
+        }
+    }
+
+    data class TableRatesDetails(
+            var tableBasedOn: RateBase? = null,
+            var rates: List<TableRate>? = null
+    ) {
+        enum class RateBase {
+            subtotal, discountedSubtotal, weight
+        }
+
+    }
+
+    data class TableRate(
+            var tableRateConditions: TableRateConditions? = null,
+            var rate: TableRateDetails? = null
+    )
+
+    data class TableRateConditions(
+            var weightFrom: Double? = null,
+            var weightTo: Double? = null,
+            var subtotalFrom: Double? = null,
+            var subtotalTo: Double? = null,
+            var discountedSubtotalFrom: Double? = null,
+            var discountedSubtotalTo: Double? = null
+    )
+
+    data class TableRateDetails(
+            var perOrderAbs: Double? = null,
+            var perOrderPercent: Double? = null,
+            var perItem: Double? = null,
+            var perWeightUnitRate: Double? = null
+    )
+
 }
