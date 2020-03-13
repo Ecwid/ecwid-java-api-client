97ab0129609c4d28ab9580109a5869e2f783f922
Igor Kurguzov <igor@ecwid.com>
2020-03-13 17:49:50 +0400

    created test for profile (update only)

---
+++ src/test/kotlin/com/ecwid/apiclient/v3/StoreProfileTest.kt
@@ -0,0 +1,163 @@
+package com.ecwid.apiclient.v3
+
+import com.ecwid.apiclient.v3.dto.profile.request.StoreProfileUpdateRequest
+import com.ecwid.apiclient.v3.dto.profile.request.UpdatedStoreProfile
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class StoreProfileTest: BaseEntityTest() {
+
+    @BeforeEach
+    override fun beforeEach() {
+        super.beforeEach()
+    }
+
+    @Test
+    fun testStoreProfile() {
+        val expectedProfile = UpdatedStoreProfile(
+                generalInfo = UpdatedStoreProfile.GeneralInfo(
+                        storeUrl = "https://www.ecwid.com",
+                        starterSite = UpdatedStoreProfile.InstantSiteInfo(
+                                ecwidSubdomain = "bobyor",
+                                customDomain = "example.com"
+                        ),
+                        websitePlatform = UpdatedStoreProfile.WebsitePlatform.wordpress
+                ),
+                account = UpdatedStoreProfile.Account(
+                        accountName = "The Bobyør",
+                        accountNickName = "bobyør",
+                        accountEmail = "bobyor@example.com"
+                ),
+                settings = UpdatedStoreProfile.Settings(
+                        closed = true,
+                        storeName = "The Shop",
+                        storeDescription = "The Store Description",
+                        googleRemarketingEnabled = true,
+                        googleAnalyticsId = "googleAnalyticsId",
+                        fbPixelId = "fbPixelId",
+                        orderCommentsEnabled = true,
+                        orderCommentsCaption = "orderCommentsCaption",
+                        orderCommentsRequired = true,
+                        hideOutOfStockProductsInStorefront = true,
+                        askCompanyName = true,
+                        favoritesEnabled = true,
+                        defaultProductSortOrder = UpdatedStoreProfile.ProductSortOrder.NAME_ASC,
+                        abandonedSales = UpdatedStoreProfile.AbandonedSalesSettings(
+                                autoAbandonedSalesRecovery = true
+                        ),
+                        salePrice = UpdatedStoreProfile.SalePriceSettings(
+                                displayOnProductList = true,
+                                oldPriceLabel = "oldPriceLabel",
+                                displayDiscount = UpdatedStoreProfile.SalePriceSettings.DisplayDiscount.PERCENT
+                        ),
+                        showAcceptMarketingCheckbox = true,
+                        acceptMarketingCheckboxDefaultValue = true,
+                        acceptMarketingCheckboxCustomText = "acceptMarketingCheckboxCustomText",
+                        askConsentToTrackInStorefront = true,
+                        snapPixelId = "snapPixelId",
+                        pinterestTagId = "pinterestTagId",
+                        googleTagId = "googleTagId",
+                        googleEventId = "googleEventId"
+                ),
+                mailNotifications = UpdatedStoreProfile.MailNotifications(
+                        adminNotificationEmails = listOf("admin@example.com", "admin2@example.com"),
+                        customerNotificationFromEmail = "customerfrom@example.com"
+                ),
+                company = UpdatedStoreProfile.Company(
+                        companyName = "companyName",
+                        email = "email@example.com",
+                        street = "street",
+                        city = "city",
+                        countryCode = "RU",
+                        postalCode = "postalCode",
+                        phone = "phone",
+                        stateOrProvinceCode = "73"
+                ),
+                formatsAndUnits = UpdatedStoreProfile.FormatsAndUnits(
+                        currency = "currency",
+                        currencyPrefix = "currencyPrefix",
+                        currencySuffix = "currencySuffix",
+                        currencyGroupSeparator = "G",
+                        currencyDecimalSeparator = "D",
+                        currencyTruncateZeroFractional = true,
+                        currencyRate = 1.23,
+                        weightUnit = UpdatedStoreProfile.WeightUnit.GRAM,
+                        weightPrecision = 2,
+                        weightGroupSeparator = "R",
+                        weightDecimalSeparator = "E",
+                        weightTruncateZeroFractional = true,
+                        dateFormat = "yyyy/MM/dd",
+                        timeFormat = "HH:mm:ss",
+                        timezone = "timezone",
+                        dimensionsUnit = UpdatedStoreProfile.DimensionUnit.MM,
+                        orderNumberPrefix = "orderNumberPrefix",
+                        orderNumberSuffix = "orderNumberSuffix"
+                ),
+                languages = UpdatedStoreProfile.Languages(
+                        enabledLanguages = listOf("en", "ru"),
+                        defaultLanguage = "ru"
+                ),
+                shipping = UpdatedStoreProfile.Shipping(
+                        handlingFee = UpdatedStoreProfile.HandlingFee(
+                                name = "fee",
+                                value = 123.0,
+                                description = "fee descr"
+                        )
+                ),
+                taxSettings = UpdatedStoreProfile.TaxSettings(
+                        automaticTaxEnabled = true,
+                        taxes = listOf(UpdatedStoreProfile.TaxSettings.Taxes(
+                                id = 100,
+                                name = "tax-name",
+                                enabled = true,
+                                includeInPrice = true,
+                                useShippingAddress = true,
+                                taxShipping = true,
+                                appliedByDefault = true,
+// TODO: uncomment after bug ECWID-65852 is fixed
+//                                rules = listOf(UpdatedStoreProfile.TaxSettings.TaxRule(
+//                                        zoneId = "zoneId",
+//                                        tax = 14.0
+//                                )),
+                                defaultTax = 12.0
+                        ))
+                ),
+// TODO: uncomment after bug ECWID-65838 is fixed
+//                zones = listOf(UpdatedStoreProfile.Zone(
+//                        id = "zoneId",
+//                        name = "Zone name",
+//                        countryCodes = listOf("RU", "UA"),
+//                        stateOrProvinceCodes = listOf("73", "77"),
+//                        postCodes = listOf("123456", "654321")
+//                )),
+                businessRegistrationID = UpdatedStoreProfile.BusinessRegistrationID(
+                        name = "businessRegistrationID",
+                        value = "busines-value"
+                ),
+                legalPagesSettings = UpdatedStoreProfile.LegalPagesSettingsDetails(
+                        requireTermsAgreementAtCheckout = true,
+                        legalPages = listOf(UpdatedStoreProfile.LegalPagesInfo(
+                                type = UpdatedStoreProfile.LegalPagesInfo.Type.LEGAL_INFO,
+                                enabled = true,
+                                title = "page title",
+                                display = UpdatedStoreProfile.LegalPagesInfo.Display.EXTERNAL_URL,
+                                text = "page text",
+                                externalUrl = "page url"
+                        ))
+                ),
+                orderInvoiceSettings = UpdatedStoreProfile.OrderInvoiceSettings(
+                        displayOrderInvoices = true,
+                        attachInvoiceToOrderEmailNotifications = UpdatedStoreProfile.OrderInvoiceSettings.AttachValue.ATTACH_TO_ALL_EMAILS,
+                        invoiceLogoUrl = "invoice.logo.url"
+                )
+        )
+
+        val updateResult = apiClient.updateStoreProfile(StoreProfileUpdateRequest(expectedProfile))
+
+        assertTrue(updateResult.success)
+        assertEquals(1, updateResult.updateCount)
+    }
+
+}
