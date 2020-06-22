bf207ae35ccb275e4c66e67c3fadea69bd4616f5
baskak <baskak@ecwid.com>
2020-06-22 15:04:52 +0400

    ECWID-69644: added support for Profile.designSettings field

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/FetchedStoreProfile.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/FetchedStoreProfile.kt
@@ -1,5 +1,7 @@
 package com.ecwid.apiclient.v3.dto.profile.result
 
+import com.google.gson.annotations.SerializedName
+
 data class FetchedStoreProfile(
 		var generalInfo: GeneralInfo? = null,
 		var account: Account? = null,
@@ -15,6 +17,7 @@ data class FetchedStoreProfile(
 		var legalPagesSettings: LegalPagesSettingsDetails? = null,
 		var payment: PaymentInfo? = null,
 		var featureToggles: List<FeatureTogglesInfo>? = null,
+		var designSettings: DesignSettings? = null,
 		var productFiltersSettings: ProductFiltersSettings? = null,
 		var fbMessengerSettings: FBMessengerSettings? = null,
 		var orderInvoiceSettings: OrderInvoiceSettings? = null,
@@ -350,6 +353,215 @@ data class FetchedStoreProfile(
 			var name: String? = null,
 			var visible: Boolean? = null,
 			var enabled: Boolean? = null
+	)
+
+	data class DesignSettings(
+			@SerializedName("enable_catalog_on_one_page")
+			var enableCatalogOnOnePage: Boolean? = null,
+
+			@SerializedName("product_list_image_size")
+			var productListImageSize: String? = null,
+
+			@SerializedName("product_list_image_aspect_ratio")
+			var productListImageLayout: String? = null,
+
+			@SerializedName("product_list_image_position")
+			var productListImagePosition: String? = null,
+
+			@SerializedName("product_list_category_image_size")
+			var productListCategoryImageSize: String? = null,
+
+			@SerializedName("product_list_category_image_aspect_ratio")
+			var productListCategoryImageLayout: String? = null,
+
+			@SerializedName("product_list_category_image_position")
+			var productListCategoryImagePosition: String? = null,
+
+			@SerializedName("product_list_show_product_images")
+			var productListShowProductImages: Boolean? = null,
+
+			@SerializedName("product_list_product_info_layout")
+			var productListCardLayout: String? = null,
+
+			@SerializedName("product_list_show_frame")
+			var productListShowCardFrame: Boolean? = null,
+
+			@SerializedName("product_list_show_additional_image_on_hover")
+			var productListShowAdditionalImage: Boolean? = null,
+
+			@SerializedName("product_list_title_behavior")
+			var productListNameBehaviour: String? = null,
+
+			@SerializedName("product_list_price_behavior")
+			var productListPriceBehaviour: String? = null,
+
+			@SerializedName("product_list_sku_behavior")
+			var productListSKUBehaviour: String? = null,
+
+			@SerializedName("product_list_buybutton_behavior")
+			var productListBuyNowBehaviour: String? = null,
+
+			@SerializedName("product_list_category_title_behavior")
+			var productListCategoryNameBehaviour: String? = null,
+
+			@SerializedName("product_list_image_has_shadow")
+			var productListImageHasShadow: Boolean? = null,
+
+			@SerializedName("show_signin_link")
+			var productListShowSignInLink: Boolean? = null,
+
+			@SerializedName("show_footer_menu")
+			var productListShowFooterMenu: Boolean? = null,
+
+			@SerializedName("show_breadcrumbs")
+			var productListShowBreadcrumbs: Boolean? = null,
+
+			@SerializedName("product_list_show_sort_viewas_options")
+			var productListShowSortViewAsOptions: Boolean? = null,
+
+			@SerializedName("product_list_show_on_sale_label")
+			var productListShowOnSaleLabel: Boolean? = null,
+
+			@SerializedName("product_list_show_sold_out_label")
+			var productListShowSoldOutLabel: Boolean? = null,
+
+			@SerializedName("product_details_show_product_sku")
+			var productDetailsShowProductSku: Boolean? = null,
+
+			@SerializedName("product_details_additional_images_has_shadow")
+			var productDetailsAdditionalImagesHasShadow: Boolean? = null,
+
+			@SerializedName("product_details_image_carousel")
+			var productDetailsImageCarousel: Boolean? = null,
+
+			@SerializedName("product_details_additional_images_preview_on_click")
+			var productDetailsAdditionalImagesPreviewOnClick: Boolean? = null,
+
+			@SerializedName("product_details_layout")
+			var productDetailsLayout: String? = null,
+
+			@SerializedName("product_details_two_columns_with_right_sidebar_show_product_description_on_sidebar")
+			var productDetailsTwoColumnsWithRightSidebarShowProductDescriptionOnSidebar: Boolean? = null,
+
+			@SerializedName("product_details_two_columns_with_left_sidebar_show_product_description_on_sidebar")
+			var productDetailsTwoColumnsWithLeftSidebarShowProductDescriptionOnSidebar: Boolean? = null,
+
+			@SerializedName("product_details_show_product_name")
+			var productDetailsShowProductName: Boolean? = null,
+
+			@SerializedName("product_details_show_breadcrumbs")
+			var productDetailsShowBreadcrumbs: Boolean? = null,
+
+			@SerializedName("product_details_show_product_price")
+			var productDetailsShowProductPrice: Boolean? = null,
+
+			@SerializedName("product_details_show_sale_price")
+			var productDetailsShowSalePrice: Boolean? = null,
+
+			@SerializedName("product_details_show_tax")
+			var productDetailsShowTax: Boolean? = null,
+
+			@SerializedName("product_details_show_product_description")
+			var productDetailsShowProductDescription: Boolean? = null,
+
+			@SerializedName("product_details_show_product_options")
+			var productDetailsShowProductOptions: Boolean? = null,
+
+			@SerializedName("product_details_show_buy_button")
+			var productDetailsShowBuyButton: Boolean? = null,
+
+			@SerializedName("product_details_show_wholesale_prices")
+			var productDetailsShowWholesalePrices: Boolean? = null,
+
+			@SerializedName("product_details_show_save_for_later")
+			var productDetailsShowSaveForLater: Boolean? = null,
+
+			@SerializedName("product_details_show_share_buttons")
+			var productDetailsShowShareButtons: Boolean? = null,
+
+			@SerializedName("product_details_show_product_name_always_first_on_mobile")
+			var productDetailsShowProductNameAlwaysFirstOnMobile: Boolean? = null,
+
+			@SerializedName("product_details_position_product_name")
+			var productDetailsPositionProductName: Int? = null,
+
+			@SerializedName("product_details_position_breadcrumbs")
+			var productDetailsPositionBreadcrumbs: Int? = null,
+
+			@SerializedName("product_details_position_product_sku")
+			var productDetailsPositionProductSku: Int? = null,
+
+			@SerializedName("product_details_position_product_price")
+			var productDetailsPositionProductPrice: Int? = null,
+
+			@SerializedName("product_details_position_product_options")
+			var productDetailsPositionProductOptions: Int? = null,
+
+			@SerializedName("product_details_position_buy_button")
+			var productDetailsPositionBuyButton: Int? = null,
+
+			@SerializedName("product_details_position_wholesale_prices")
+			var productDetailsPositionWholesalePrices: Int? = null,
+
+			@SerializedName("product_details_position_product_description")
+			var productDetailsPositionProductDescription: Int? = null,
+
+			@SerializedName("product_details_position_save_for_later")
+			var productDetailsPositionSaveForLater: Int? = null,
+
+			@SerializedName("product_details_position_share_buttons")
+			var productDetailsPositionShareButtons: Int? = null,
+
+			@SerializedName("product_details_hide_price_modifiers")
+			var productDetailsHidePriceModifiers: Boolean? = null,
+
+			@SerializedName("product_details_show_price_per_unit")
+			var productDetailsShowPricePerUnit: Boolean? = null,
+
+			@SerializedName("product_details_cut_product_description_in_sidebar")
+			var productDetailsCutProductDescriptionInSidebar: Boolean? = null,
+
+			@SerializedName("product_details_thumbnails_aspect_ratio")
+			var productDetailsThumbnailsAspectRatio: String? = null,
+
+			@SerializedName("product_details_show_qty")
+			var productDetailsShowQty: Boolean? = null,
+
+			@SerializedName("product_details_show_in_stock_label")
+			var productDetailsShowInStockLabel: Boolean? = null,
+
+			@SerializedName("product_details_show_number_of_items_in_stock")
+			var productDetailsShowNumberOfItemsInStock: Boolean? = null,
+
+			@SerializedName("product_details_gallery_layout")
+			var productDetailsGalleryLayout: String? = null,
+
+			@SerializedName("product_details_show_facebook_share_button")
+			var productDetailsShowFacebookShareButton: Boolean? = null,
+
+			@SerializedName("product_details_show_twitter_share_button")
+			var productDetailsShowTwitterShareButton: Boolean? = null,
+
+			@SerializedName("product_details_show_pinterest_share_button")
+			var productDetailsShowPinterestShareButton: Boolean? = null,
+
+			@SerializedName("product_details_show_vk_share_button")
+			var productDetailsShowVkShareButton: Boolean? = null,
+
+			@SerializedName("enable_simple_category_list")
+			var enableSimpleCategoryList: Boolean? = null,
+
+			@SerializedName("enable_page_transitions")
+			var enablePageTransitions: Boolean? = null,
+
+			@SerializedName("product_filters_opened_by_default_on_category_page")
+			var productFiltersOpenedByDefaultOnCategoryPage: Boolean? = null,
+
+			@SerializedName("product_filters_position_category_page")
+			var productFiltersPositionCategoryPage: String? = null,
+
+			@SerializedName("show_root_categories")
+			var showRootCategories: Boolean? = null
 	)
 
 	data class ProductFiltersSettings(
