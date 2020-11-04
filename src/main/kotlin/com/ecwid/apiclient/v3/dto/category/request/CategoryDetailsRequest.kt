0726c081eb1121bb5a2c4539808503be2e7af21e
kruz <kruz@ecwid.com>
2020-11-04 09:47:37 +0400

    Added query fields to get category details request

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/CategoryDetailsRequest.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/CategoryDetailsRequest.kt
@@ -4,9 +4,22 @@ import com.ecwid.apiclient.v3.dto.ApiRequest
 import com.ecwid.apiclient.v3.impl.RequestInfo
 
 data class CategoryDetailsRequest(
-		val categoryId: Int = 0
+		val categoryId: Int = 0,
+		val baseUrl: String? = null,
+		val cleanUrls: Boolean? = null,
+		val lang: String? = null
 ) : ApiRequest {
 	override fun toRequestInfo() = RequestInfo.createGetRequest(
-			endpoint = "categories/$categoryId"
+			endpoint = "categories/$categoryId",
+			params = toParams()
 	)
+
+	private fun toParams(): Map<String, String> {
+		val request = this
+		return mutableMapOf<String, String>().apply {
+			request.baseUrl?.let { put("baseUrl", it) }
+			request.cleanUrls?.let { put("cleanUrls", it.toString()) }
+			request.lang?.let { put("lang", it) }
+		}.toMap()
+	}
 }
