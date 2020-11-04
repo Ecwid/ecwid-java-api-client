a775ccb4b3bd8fdbcf88c899e4160b716abdb59a
kruz <kruz@ecwid.com>
2020-11-04 09:44:13 +0400

    Added public method for getting store update statistics

--- src/main/kotlin/com/ecwid/apiclient/v3/impl/StoreProfileApiClientImpl.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/impl/StoreProfileApiClientImpl.kt
@@ -2,8 +2,10 @@ package com.ecwid.apiclient.v3.impl
 
 import com.ecwid.apiclient.v3.ApiClientHelper
 import com.ecwid.apiclient.v3.StoreProfileApiClient
+import com.ecwid.apiclient.v3.dto.profile.request.LatestStatsRequest
 import com.ecwid.apiclient.v3.dto.profile.request.StoreProfileRequest
 import com.ecwid.apiclient.v3.dto.profile.request.StoreProfileUpdateRequest
+import com.ecwid.apiclient.v3.dto.profile.result.FetchedLatestStats
 import com.ecwid.apiclient.v3.dto.profile.result.FetchedStoreProfile
 import com.ecwid.apiclient.v3.dto.profile.result.StoreProfileUpdateResult
 
@@ -12,5 +14,6 @@ internal class StoreProfileApiClientImpl(
 ) : StoreProfileApiClient {
 	override fun getStoreProfile(request: StoreProfileRequest) = apiClientHelper.makeObjectResultRequest<FetchedStoreProfile>(request)
 	override fun updateStoreProfile(request: StoreProfileUpdateRequest) = apiClientHelper.makeObjectResultRequest<StoreProfileUpdateResult>(request)
+	override fun getLatestStats(request: LatestStatsRequest) = apiClientHelper.makeObjectResultRequest<FetchedLatestStats>(request)
 
 }
