01641ccbdce76cff14adbe92f83cd66618ec6850
Igor Kurguzov <igor@ecwid.com>
2021-03-24 16:45:05 +0400

    refactoring: moved method TransportHttpBody.toEntity into its host class

--- src/main/kotlin/com/ecwid/apiclient/v3/httptransport/impl/ApacheCommonsHttpClientTransport.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/httptransport/impl/ApacheCommonsHttpClientTransport.kt
@@ -6,13 +6,10 @@ import com.ecwid.apiclient.v3.httptransport.HttpTransport
 import com.ecwid.apiclient.v3.httptransport.TransportHttpBody
 import org.apache.http.Consts
 import org.apache.http.Header
-import org.apache.http.HttpEntity
-import org.apache.http.HttpStatus
 import org.apache.http.client.HttpClient
 import org.apache.http.client.config.RequestConfig
 import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.client.methods.RequestBuilder
-import org.apache.http.entity.*
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
 import org.apache.http.message.BasicNameValuePair
@@ -158,17 +155,4 @@ private fun createNameValuePairs(params: Map<String, String>): Array<BasicNameVa
 	return params
 			.map { (name, value) -> BasicNameValuePair(name, value) }
 			.toTypedArray()
-}
-
-private fun String.toContentType(): ContentType = ContentType.create(this, Consts.UTF_8)
-
-private fun TransportHttpBody.toEntity(): HttpEntity? = when (this) {
-	is TransportHttpBody.EmptyBody ->
-		null
-	is TransportHttpBody.InputStreamBody ->
-		BufferedHttpEntity(InputStreamEntity(stream, mimeType.toContentType()))
-	is TransportHttpBody.ByteArrayBody ->
-		ByteArrayEntity(byteArray, mimeType.toContentType())
-	is TransportHttpBody.LocalFileBody ->
-		FileEntity(file, mimeType.toContentType())
 }
