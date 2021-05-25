99f33f1599d5622fa28c687dae31e5f3114afaba
Alexey Zagaichuk <alexis@ecwid.com>
2021-05-25 14:57:19 +0400

    Fixed Detekt inspection `UtilityClassWithPublicConstructor`

--- src/test/kotlin/com/ecwid/apiclient/v3/util/PropertiesLoader.kt
+++ src/test/kotlin/com/ecwid/apiclient/v3/util/PropertiesLoader.kt
@@ -5,83 +5,83 @@ import java.util.*
 private const val DEFAULT_API_HOST = "app.ecwid.com"
 private const val DEFAULT_API_PORT = 443
 
-class PropertiesLoader {
-	companion object {
-		sealed class LoadResult {
-			data class Success(val properties: TestProperties) : LoadResult()
-			data class Failed(val message: String) : LoadResult()
-		}
+object PropertiesLoader {
 
-		private val lazyLoadProperties = lazy {
-			val envProperties = fromEnv()
-			if (envProperties != null) {
-				LoadResult.Success(envProperties)
-			} else {
-				fromResource("/test.properties")
-			}
-		}
+	private sealed class LoadResult {
+		data class Success(val properties: TestProperties) : LoadResult()
+		data class Failed(val message: String) : LoadResult()
+	}
 
-		fun load(): TestProperties {
-			return when (val loadResult = lazyLoadProperties.value) {
-				is LoadResult.Success -> loadResult.properties
-				is LoadResult.Failed -> throw PropertiesLoadError(loadResult.message)
-			}
-		}
-
-		private fun fromEnv(): TestProperties? {
-			val apiHost = System.getenv("API_HOST") ?: DEFAULT_API_HOST
-			val storeId = System.getenv("STORE_ID")?.toIntOrNull()
-					?: return null
-			val apiToken = System.getenv("API_TOKEN")
-					?: return null
-
-			return TestProperties(
-					apiHost = apiHost,
-					apiPort = DEFAULT_API_PORT,
-					storeId = storeId,
-					apiToken = apiToken
-			)
-		}
-
-		private fun fromResource(resourceName: String): LoadResult {
-			val resource = PropertiesLoader::class.java.getResourceAsStream(resourceName)
-					?: return LoadResult.Failed("File $resourceName not found in test/resources dir. Please copy test/resources/test.properties.sample and configure it")
-
-			val p = Properties()
-			p.load(resource)
-
-			val apiHostStr = p.getProperty("apiHost")
-			val apiHost = if (apiHostStr.isNullOrEmpty()) {
-				DEFAULT_API_HOST
-			} else {
-				apiHostStr
-			}
-
-			val apiPort = p.getProperty("apiPort")?.toIntOrNull() ?: DEFAULT_API_PORT
-
-			val storeIdStr = p.getProperty("storeId")
-			if (storeIdStr.isNullOrEmpty()) {
-				return LoadResult.Failed("Parameter storeId is required")
-			}
-
-			val apiToken = p.getProperty("apiToken")
-			if (apiToken.isNullOrEmpty()) {
-				return LoadResult.Failed("Parameter apiToken is required")
-			}
-
-			val storeId = storeIdStr.toIntOrNull()
-			if (storeId == null || storeId <= 0) {
-				return LoadResult.Failed("Parameter storeId must be a valid Store ID")
-			}
-
-			val testProperties = TestProperties(
-					apiHost = apiHost,
-					apiPort = apiPort,
-					storeId = storeId,
-					apiToken = apiToken
-			)
-
-			return LoadResult.Success(testProperties)
+	private val lazyLoadProperties = lazy {
+		val envProperties = fromEnv()
+		if (envProperties != null) {
+			LoadResult.Success(envProperties)
+		} else {
+			fromResource("/test.properties")
 		}
 	}
+
+	fun load(): TestProperties {
+		return when (val loadResult = lazyLoadProperties.value) {
+			is LoadResult.Success -> loadResult.properties
+			is LoadResult.Failed -> throw PropertiesLoadError(loadResult.message)
+		}
+	}
+
+	private fun fromEnv(): TestProperties? {
+		val apiHost = System.getenv("API_HOST") ?: DEFAULT_API_HOST
+		val storeId = System.getenv("STORE_ID")?.toIntOrNull()
+				?: return null
+		val apiToken = System.getenv("API_TOKEN")
+				?: return null
+
+		return TestProperties(
+				apiHost = apiHost,
+				apiPort = DEFAULT_API_PORT,
+				storeId = storeId,
+				apiToken = apiToken
+		)
+	}
+
+	private fun fromResource(resourceName: String): LoadResult {
+		val resource = PropertiesLoader::class.java.getResourceAsStream(resourceName)
+				?: return LoadResult.Failed("File $resourceName not found in test/resources dir. Please copy test/resources/test.properties.sample and configure it")
+
+		val p = Properties()
+		p.load(resource)
+
+		val apiHostStr = p.getProperty("apiHost")
+		val apiHost = if (apiHostStr.isNullOrEmpty()) {
+			DEFAULT_API_HOST
+		} else {
+			apiHostStr
+		}
+
+		val apiPort = p.getProperty("apiPort")?.toIntOrNull() ?: DEFAULT_API_PORT
+
+		val storeIdStr = p.getProperty("storeId")
+		if (storeIdStr.isNullOrEmpty()) {
+			return LoadResult.Failed("Parameter storeId is required")
+		}
+
+		val apiToken = p.getProperty("apiToken")
+		if (apiToken.isNullOrEmpty()) {
+			return LoadResult.Failed("Parameter apiToken is required")
+		}
+
+		val storeId = storeIdStr.toIntOrNull()
+		if (storeId == null || storeId <= 0) {
+			return LoadResult.Failed("Parameter storeId must be a valid Store ID")
+		}
+
+		val testProperties = TestProperties(
+				apiHost = apiHost,
+				apiPort = apiPort,
+				storeId = storeId,
+				apiToken = apiToken
+		)
+
+		return LoadResult.Success(testProperties)
+	}
+
 }
