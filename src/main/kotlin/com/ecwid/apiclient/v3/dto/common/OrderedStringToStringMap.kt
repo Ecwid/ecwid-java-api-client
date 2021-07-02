e61aaa1b6f50e0dc2fb65f8d8fa0dd57bbdce9ff
Igor Kurguzov <igor@ecwid.com>
2021-07-02 10:00:31 +0400

    changed structure of MerchantAppSettings DTO to match json serialization structure

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/common/OrderedStringToStringMap.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/common/OrderedStringToStringMap.kt
@@ -2,14 +2,14 @@ package com.ecwid.apiclient.v3.dto.common
 
 import java.util.*
 
-class OrderedStringToStringMap : TreeMap<String, String> {
+open class OrderedStringToStringMap : TreeMap<String, String> {
 
 	@Suppress("unused")
 	constructor() : super()
 
 	constructor(map: Map<String, String>?) {
 		if (map != null) {
-			putAll(map)
+			super.putAll(map)
 		}
 	}
 
