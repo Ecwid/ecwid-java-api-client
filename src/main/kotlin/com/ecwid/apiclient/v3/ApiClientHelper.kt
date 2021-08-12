34c520ef036d577a06f45c92abe7a492c1c015f9
Igor Kurguzov <igor@ecwid.com>
2021-08-12 08:21:36 +0400

    got rid of magic number

--- src/main/kotlin/com/ecwid/apiclient/v3/ApiClientHelper.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/ApiClientHelper.kt
@@ -24,6 +24,7 @@ private const val APP_CLIENT_ID_PARAM_NAME = "appClientId"
 private const val APP_CLIENT_SECRET_PARAM_NAME = "appSecretKey"
 
 private const val REQUEST_ID_LENGTH = 8
+private const val MAX_LOG_ENTRY_SECTION_LENGTH = 200
 private val REQUEST_ID_CHARACTERS = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 
 class ApiClientHelper private constructor(
@@ -310,10 +311,11 @@ class ApiClientHelper private constructor(
 		logLevel: Level?,
 		requestId: String,
 		sections: List<String>,
-		exception: Exception? = null
+		exception: Exception? = null,
+		maxSectionLength: Int = MAX_LOG_ENTRY_SECTION_LENGTH
 	) {
 		val sectionsString = sections.joinToString(separator = "; ") {
-			if (it.length > 200) it.take(200) + "…" else it
+			if (it.length > maxSectionLength) it.take(maxSectionLength) + "…" else it
 		}
 		val logMessage = "$prefix [$requestId]: $sectionsString"
 		if (exception != null) {
