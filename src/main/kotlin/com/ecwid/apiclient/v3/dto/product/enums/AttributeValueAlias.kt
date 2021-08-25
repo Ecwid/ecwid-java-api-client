38d36e231adad8118a7190034f298456c3a372d1
Vasily Karyaev <basil@ecwid.com>
2021-08-25 11:39:54 +0400

    Extend AttributeValueAlias with PPU attributes

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/product/enums/AttributeValueAlias.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/product/enums/AttributeValueAlias.kt
@@ -4,13 +4,17 @@ import com.ecwid.apiclient.v3.dto.producttype.enums.AttributeType
 
 enum class AttributeValueAlias {
 	UPC,
-	BRAND
+	BRAND,
+	PRICE_PER_UNIT,
+	UNITS_IN_PRODUCT,
 }
 
 fun AttributeType.toAttributeValueAlias(): AttributeValueAlias? {
 	return when (this) {
 		AttributeType.UPC -> AttributeValueAlias.UPC
 		AttributeType.BRAND -> AttributeValueAlias.BRAND
+		AttributeType.PRICE_PER_UNIT -> AttributeValueAlias.PRICE_PER_UNIT
+		AttributeType.UNITS_IN_PRODUCT -> AttributeValueAlias.UNITS_IN_PRODUCT
 		else -> null
 	}
 }
