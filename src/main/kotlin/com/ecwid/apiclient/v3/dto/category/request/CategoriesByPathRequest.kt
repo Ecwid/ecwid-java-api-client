8bbccd0a8291212aebfd3b66b79578bd39a781bd
Vladimir Romanov <garnet@ecwid.com>
2021-09-07 16:39:09 +0400

    Fix colon and const

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/CategoriesByPathRequest.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/CategoriesByPathRequest.kt
@@ -3,13 +3,14 @@ package com.ecwid.apiclient.v3.dto.category.request
 import com.ecwid.apiclient.v3.dto.ApiRequest
 import com.ecwid.apiclient.v3.impl.RequestInfo
 
+const val DELIMITER_LENGTH = 6
+
 data class CategoriesByPathRequest(
 	val path: List<String>,
 	val offset: Int = 0,
 	val limit: Int = 100,
 	val lang: String? = null
 ) : ApiRequest {
-
 	override fun toRequestInfo() = RequestInfo.createGetRequest(
 		pathSegments = listOf(
 			"categoriesByPath"
@@ -19,7 +20,7 @@ data class CategoriesByPathRequest(
 
 	private fun toParams(): Map<String, String> {
 		val request = this
-		val randomDelimiter = "{" + generateRandomUID(6) + "}"
+		val randomDelimiter = "{" + generateRandomUID(DELIMITER_LENGTH) + "}"
 		val generatedPath = path.joinToString(randomDelimiter)
 
 		return mutableMapOf<String, String>().apply {
@@ -32,7 +33,7 @@ data class CategoriesByPathRequest(
 	}
 }
 
-private fun generateRandomUID(length : Int) : String {
+private fun generateRandomUID(length: Int): String {
 	val alphabet = "0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
 	return (1..length).joinToString("") { alphabet.random().toString() }
 }
