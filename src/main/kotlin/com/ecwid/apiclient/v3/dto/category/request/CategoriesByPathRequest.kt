10ba9561ba91f64efe9236228fb907e28a91eff6
Vladimir Romanov <garnet@ecwid.com>
2021-09-09 09:22:50 +0400

    Add secondary constructor

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/CategoriesByPathRequest.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/category/request/CategoriesByPathRequest.kt
@@ -3,17 +3,38 @@ package com.ecwid.apiclient.v3.dto.category.request
 import com.ecwid.apiclient.v3.dto.ApiRequest
 import com.ecwid.apiclient.v3.impl.RequestInfo
 
-private const val DELIMITER_LENGTH = 6
+/**
+ * Weird delimiter to ensure it never occurs in category names.
+ */
+private const val DELIMITER = "Ω≈ç√∫˜˜≤≥çæ…¬˚∆˙"
 
 data class CategoriesByPathRequest(
 	/**
-	 * List of category path elements
+	 * Full category path with delimiter
 	 */
-	val path: List<String> = listOf(),
+	val path: String = "",
+	val delimiter: String = "",
 	val offset: Int = 0,
 	val limit: Int = 100,
 	val lang: String? = null
 ) : ApiRequest {
+
+	constructor(
+		/**
+		 * List of category path elements
+		 */
+		pathElements: List<String>,
+		offset: Int,
+		limit: Int,
+		lang: String?
+	) : this(
+		path = pathElements.joinToString(DELIMITER),
+		delimiter = DELIMITER,
+		offset = offset,
+		limit = limit,
+		lang = lang
+	)
+
 	override fun toRequestInfo() = RequestInfo.createGetRequest(
 		pathSegments = listOf(
 			"categoriesByPath"
@@ -23,20 +44,13 @@ data class CategoriesByPathRequest(
 
 	private fun toParams(): Map<String, String> {
 		val request = this
-		val randomDelimiter = "{" + generateRandomUID(DELIMITER_LENGTH) + "}"
-		val generatedPath = path.joinToString(randomDelimiter)
 
 		return mutableMapOf<String, String>().apply {
-			put("path", generatedPath)
-			put("delimiter", randomDelimiter)
+			put("path", path)
+			put("delimiter", delimiter)
 			put("offset", request.offset.toString())
 			put("limit", request.limit.toString())
 			request.lang?.let { put("lang", it) }
 		}.toMap()
 	}
-}
-
-private fun generateRandomUID(length: Int): String {
-	val alphabet = "0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
-	return (1..length).joinToString("") { alphabet.random().toString() }
 }
