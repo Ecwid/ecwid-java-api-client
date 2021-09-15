c705fd1dc10f63d4188f64ed386ce637b1765a00
Igor Kurguzov <igor@ecwid.com>
2021-09-15 15:41:08 +0400

    created requests and results for removing profile uploads

---
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/request/InvoiceLogoRemoveRequest.kt
@@ -0,0 +1,13 @@
+package com.ecwid.apiclient.v3.dto.profile.request
+
+import com.ecwid.apiclient.v3.dto.ApiRequest
+import com.ecwid.apiclient.v3.impl.RequestInfo
+
+class InvoiceLogoRemoveRequest : ApiRequest {
+	override fun toRequestInfo() = RequestInfo.createDeleteRequest(
+		pathSegments = listOf(
+			"profile",
+			"invoicelogo"
+		)
+	)
+}
