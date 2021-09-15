99a5a9510cda7f6387d3af701dba24ca14450116
Igor Kurguzov <igor@ecwid.com>
2021-09-15 15:19:32 +0400

    refactoring: extracted common build request info operation into DtoUtils

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/product/request/ProductGalleryImageUploadRequest.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/product/request/ProductGalleryImageUploadRequest.kt
@@ -2,8 +2,7 @@ package com.ecwid.apiclient.v3.dto.product.request
 
 import com.ecwid.apiclient.v3.dto.ApiRequest
 import com.ecwid.apiclient.v3.dto.common.UploadFileData
-import com.ecwid.apiclient.v3.httptransport.HttpBody
-import com.ecwid.apiclient.v3.impl.MIME_TYPE_OCTET_STREAM
+import com.ecwid.apiclient.v3.dto.common.buildUploadRequestInfo
 import com.ecwid.apiclient.v3.impl.RequestInfo
 
 data class ProductGalleryImageUploadRequest(
@@ -16,39 +15,7 @@ data class ProductGalleryImageUploadRequest(
 			"fileName" to fileName
 		)
 
-		return when (fileData) {
-			is UploadFileData.ExternalUrlData -> RequestInfo.createPostRequest(
-				pathSegments = pathSegments,
-				params = commonParams + mapOf(
-					"externalUrl" to fileData.externalUrl
-				),
-				httpBody = HttpBody.EmptyBody
-			)
-			is UploadFileData.ByteArrayData -> RequestInfo.createPostRequest(
-				pathSegments = pathSegments,
-				params = commonParams,
-				httpBody = HttpBody.ByteArrayBody(
-					bytes = fileData.bytes,
-					mimeType = MIME_TYPE_OCTET_STREAM
-				)
-			)
-			is UploadFileData.LocalFileData -> RequestInfo.createPostRequest(
-				pathSegments = pathSegments,
-				params = commonParams,
-				httpBody = HttpBody.LocalFileBody(
-					file = fileData.file,
-					mimeType = MIME_TYPE_OCTET_STREAM
-				)
-			)
-			is UploadFileData.InputStreamData -> RequestInfo.createPostRequest(
-				pathSegments = pathSegments,
-				params = commonParams,
-				httpBody = HttpBody.InputStreamBody(
-					stream = fileData.stream,
-					mimeType = MIME_TYPE_OCTET_STREAM
-				)
-			)
-		}
+		return buildUploadRequestInfo(pathSegments, commonParams, fileData)
 	}
 
 	private val pathSegments = listOf(
