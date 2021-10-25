e8950efedcf6d20954a6e7f55188b7b89949443f
Igor Kurguzov <igor@ecwid.com>
2021-10-25 10:11:02 +0400

    created NoRetryRateLimitRetryStrategy

---
+++ src/main/kotlin/com/ecwid/apiclient/v3/httptransport/impl/NoRetryRateLimitRetryStrategy.kt
@@ -0,0 +1,11 @@
+package com.ecwid.apiclient.v3.httptransport.impl
+
+import com.ecwid.apiclient.v3.httptransport.HttpRequest
+import com.ecwid.apiclient.v3.httptransport.HttpResponse
+import org.apache.http.client.HttpClient
+
+class NoRetryRateLimitRetryStrategy : RateLimitRetryStrategy {
+	override fun makeHttpRequest(httpClient: HttpClient, httpRequest: HttpRequest): HttpResponse {
+		return httpClient.execute(httpRequest.toHttpUriRequest()).toApiResponse()
+	}
+}
