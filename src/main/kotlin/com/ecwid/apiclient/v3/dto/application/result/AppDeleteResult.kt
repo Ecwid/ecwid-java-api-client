44016c0b4eca483a0bd4d47c7871ae4234918df5
born <born@ecwid.com>
2021-12-06 17:04:22 +0400

    Proposed the potential ability to delete app via API. Please review.

---
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/application/result/AppDeleteResult.kt
@@ -0,0 +1,8 @@
+package com.ecwid.apiclient.v3.dto.application.result
+
+import com.ecwid.apiclient.v3.dto.common.ApiResultDTO
+
+class AppDeleteResult(
+	val clientId: String,
+	val namespace: String,
+) : ApiResultDTO
