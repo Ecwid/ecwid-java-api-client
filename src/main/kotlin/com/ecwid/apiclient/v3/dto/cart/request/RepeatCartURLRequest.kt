957071ebf809793e0aedce2d449cf80b55498e55
Ivan Zagaichuk <elvis@ecwid.com>
2022-11-18 10:46:03 +0400

    made the class as a data class

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/cart/request/RepeatCartURLRequest.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/cart/request/RepeatCartURLRequest.kt
@@ -3,7 +3,7 @@ package com.ecwid.apiclient.v3.dto.cart.request
 import com.ecwid.apiclient.v3.dto.ApiRequest
 import com.ecwid.apiclient.v3.impl.RequestInfo
 
-class RepeatCartURLRequest(
+data class RepeatCartURLRequest(
 	val orderNumber: Long = 0
 ) : ApiRequest {
 	override fun toRequestInfo() = RequestInfo.createGetRequest(
