c21e9d41854d882d92c58c0664e409192c57b8b4
Alexey Zagaichuk <alexis@ecwid.com>
2022-11-28 12:43:18 +0400

    Update uk.co.jemos.podam:podam from version 7.2.6.RELEASE to 7.2.11.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
 	testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 	testImplementation("org.reflections:reflections:0.10.2")
-	testImplementation("uk.co.jemos.podam:podam:7.2.6.RELEASE")
+	testImplementation("uk.co.jemos.podam:podam:7.2.11.RELEASE")
 
 	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
 }
