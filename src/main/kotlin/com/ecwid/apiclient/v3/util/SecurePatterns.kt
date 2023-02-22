6638156c7796e41dd5bbb8e93337d88b1af291f1
Alexey Zagaichuk <alexis@ecwid.com>
2023-02-22 12:04:51 +0400

    Replace maskApiToken and maskAppSecretKey with more generic function maskLogString

---
+++ src/main/kotlin/com/ecwid/apiclient/v3/util/SecurePatterns.kt
@@ -0,0 +1,29 @@
+package com.ecwid.apiclient.v3.util
+
+import com.ecwid.apiclient.v3.API_TOKEN_PARAM_NAME
+import com.ecwid.apiclient.v3.APP_CLIENT_SECRET_PARAM_NAME
+import com.ecwid.apiclient.v3.config.LoggingSettings
+
+private const val PARAM_VALUE_PATTERN = "([^;,)]+)"
+
+private val API_TOKEN_SECURE_PATTERN = SecurePattern(
+	regex = Regex("$API_TOKEN_PARAM_NAME=(?:secret_|public_|)$PARAM_VALUE_PATTERN"),
+	unmaskedLength = 6
+)
+
+private val API_SECRET_KEY_SECURE_PATTERN = SecurePattern(
+	regex = Regex("$APP_CLIENT_SECRET_PARAM_NAME=$PARAM_VALUE_PATTERN"),
+	unmaskedLength = 6
+)
+
+private val GLOBAL_SECURE_PATTERNS = listOf<SecurePattern>()
+
+fun createSecurePatterns(loggingSettings: LoggingSettings) = mutableListOf<SecurePattern>().apply {
+	if (loggingSettings.maskRequestApiToken) {
+		add(API_TOKEN_SECURE_PATTERN)
+	}
+	if (loggingSettings.maskRequestApiSecretKey) {
+		add(API_SECRET_KEY_SECURE_PATTERN)
+	}
+	addAll(GLOBAL_SECURE_PATTERNS)
+}.toList()
