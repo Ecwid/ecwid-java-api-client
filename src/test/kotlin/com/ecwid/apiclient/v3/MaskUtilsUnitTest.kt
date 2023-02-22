f0213c0e76c5904912e0885dff9123bc391b21e6
Alexey Zagaichuk <alexis@ecwid.com>
2023-02-22 14:01:00 +0400

    Add masking sensitive data in JSON data

--- src/test/kotlin/com/ecwid/apiclient/v3/MaskUtilsUnitTest.kt
+++ src/test/kotlin/com/ecwid/apiclient/v3/MaskUtilsUnitTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test
 class MaskUtilsUnitTest {
 
 	@Test
-	fun testMaskLogString() {
+	fun testMaskLogKeyValueString() {
 		val logString =
 			"token=secret_RandomToken0jwOrgYc5sSKBYcvO0DbP; PasswordCredentials(email=test@example.com, password=123456)"
 		val securePatterns = listOf(
@@ -20,6 +20,23 @@ class MaskUtilsUnitTest {
 
 		val maskedLogString = logString.maskLogString(securePatterns)
 		val expectedMaskedLogString = "token=secret_Ran***DbP; PasswordCredentials(email=te***om, password=1***6)"
+		assertEquals(expectedMaskedLogString, maskedLogString)
+	}
+
+	@Test
+	fun testMaskLogJsonString() {
+		val logString =
+			"""{"billingPerson":{"email":"alexis@ecwid.com","firstName":"John","lastName":"Smith","phone":"123467890"}}"""
+		val securePatterns = listOf(
+			SecurePattern(Regex(""""email":\s*"([^"]*)""""), 6),
+			SecurePattern(Regex(""""firstName":\s*"([^"]*)""""), 6),
+			SecurePattern(Regex(""""lastName":\s*"([^"]*)""""), 6),
+			SecurePattern(Regex(""""phone":\s*"([^"]*)""""), 6),
+		)
+
+		val maskedLogString = logString.maskLogString(securePatterns)
+		val expectedMaskedLogString =
+			"""{"billingPerson":{"email":"ale***com","firstName":"***","lastName":"***","phone":"***"}}"""
 		assertEquals(expectedMaskedLogString, maskedLogString)
 	}
 
