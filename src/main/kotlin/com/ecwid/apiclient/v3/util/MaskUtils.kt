932d5c3b0df58c0028c00bc07590ef8f903ec397
Alexey Zagaichuk <alexis@ecwid.com>
2023-02-22 11:19:10 +0400

    Add maskSensitive function and unit test for it

--- src/main/kotlin/com/ecwid/apiclient/v3/util/MaskUtils.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/util/MaskUtils.kt
@@ -1,5 +1,8 @@
 package com.ecwid.apiclient.v3.util
 
+import kotlin.math.max
+import kotlin.math.min
+
 private const val TOKEN_UNMASKED_LENGTH = 3
 
 internal fun maskApiToken(apiToken: String): String {
@@ -20,8 +23,32 @@ internal fun maskApiToken(apiToken: String): String {
 
 internal fun maskAppSecretKey(secretKey: String): String {
 	return if (secretKey.length > 2 * TOKEN_UNMASKED_LENGTH) {
-		secretKey.take(TOKEN_UNMASKED_LENGTH) + "***" + secretKey.takeLast(minOf(TOKEN_UNMASKED_LENGTH, secretKey.length - 2 * TOKEN_UNMASKED_LENGTH))
+		secretKey.take(TOKEN_UNMASKED_LENGTH) + "***" + secretKey.takeLast(
+			minOf(
+				TOKEN_UNMASKED_LENGTH,
+				secretKey.length - 2 * TOKEN_UNMASKED_LENGTH
+			)
+		)
 	} else {
 		"***"
 	}
+}
+
+fun maskSensitive(sensitiveData: String?, unmaskedLength: Int): String {
+	if (sensitiveData == null) {
+		return ""
+	}
+
+	if (sensitiveData.length - unmaskedLength < 4) {
+		return "***"
+	}
+
+	var maskLength = sensitiveData.length - min(unmaskedLength, sensitiveData.length)
+	maskLength = max(maskLength, sensitiveData.length / 2)
+
+	val maskedFirst = (sensitiveData.length - maskLength) / 2
+	val maskedLast = maskedFirst + maskLength
+	val prefix = sensitiveData.substring(0, maskedFirst)
+	val suffix = sensitiveData.substring(maskedLast)
+	return "$prefix***$suffix"
 }
