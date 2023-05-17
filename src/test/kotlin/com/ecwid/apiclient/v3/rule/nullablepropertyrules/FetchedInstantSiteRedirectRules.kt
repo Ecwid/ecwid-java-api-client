e5f05c7aaa4e84bf92ef918bb5b7754a24069cbc
Andrey Samoylenko <andrey.samoylenko@lightspeedhq.com>
2023-05-17 12:22:56 +0400

    ECWID-113720 Generic redirects for Instant Sites: CSV Import for redirects: Removed useless class

--- src/test/kotlin/com/ecwid/apiclient/v3/rule/nullablepropertyrules/FetchedInstantSiteRedirectRules.kt
+++
@@ -1,5 +0,0 @@
-package com.ecwid.apiclient.v3.rule.nullablepropertyrules
-
-import com.ecwid.apiclient.v3.rule.NullablePropertyRule
-
-val fetchedInstantSiteRedirectNullablePropertyRules: List<NullablePropertyRule<*, *>> = listOf()
