81207956540030f524b38c5aea46c44a89ca29e5
Andrey Samoylenko <andrey.samoylenko@lightspeedhq.com>
2023-05-22 17:57:17 +0400

    ECWID-113720 Generic redirects for Instant Sites: CSV Import for redirects: Removed useless import

--- src/test/kotlin/com/ecwid/apiclient/v3/entity/CustomersTest.kt
+++ src/test/kotlin/com/ecwid/apiclient/v3/entity/CustomersTest.kt
@@ -15,7 +15,6 @@ import com.ecwid.apiclient.v3.util.randomBoolean
 import com.ecwid.apiclient.v3.util.randomEmail
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.util.*
 
