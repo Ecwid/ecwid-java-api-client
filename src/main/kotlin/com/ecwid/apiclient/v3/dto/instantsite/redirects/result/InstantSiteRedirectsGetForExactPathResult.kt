fae3dbde3fb994909b8f0002963ff180b9df1220
Andrey Samoylenko <andrey.samoylenko@lightspeedhq.com>
2023-10-05 12:56:56 +0400

    ECWID-127444 Instant Site Redirects: added ApiResultDTO implementation to InstantSiteRedirectsGetForExactPathResult data class

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/instantsite/redirects/result/InstantSiteRedirectsGetForExactPathResult.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/instantsite/redirects/result/InstantSiteRedirectsGetForExactPathResult.kt
@@ -1,6 +1,8 @@
 package com.ecwid.apiclient.v3.dto.instantsite.redirects.result
 
+import com.ecwid.apiclient.v3.dto.common.ApiResultDTO
+
 data class InstantSiteRedirectsGetForExactPathResult(
 	val items: List<FetchedInstantSiteRedirect> = listOf(),
 	val hasAnyRedirects: Boolean = true,
-)
+) : ApiResultDTO
