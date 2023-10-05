dc71687523a9b253c2a15efd157926a18227402e
Andrey Samoylenko <andrey.samoylenko@lightspeedhq.com>
2023-10-05 11:18:22 +0400

    ECWID-127444 Instant Site Redirects: Replaced var with val for hasAnyRedirects param

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/instantsite/redirects/result/InstantSiteRedirectsGetForExactPathResult.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/instantsite/redirects/result/InstantSiteRedirectsGetForExactPathResult.kt
@@ -2,5 +2,5 @@ package com.ecwid.apiclient.v3.dto.instantsite.redirects.result
 
 data class InstantSiteRedirectsGetForExactPathResult(
 	val items: List<FetchedInstantSiteRedirect> = listOf(),
-	var hasAnyRedirects: Boolean = true,
+	val hasAnyRedirects: Boolean = true,
 )
