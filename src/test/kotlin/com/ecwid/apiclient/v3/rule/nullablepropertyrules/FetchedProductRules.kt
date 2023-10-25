d914c42fcf026623b78b32b9f07e042f5711414b
Andrey Samoylenko <andrey.samoylenko@lightspeedhq.com>
2023-10-25 18:06:53 +0400

    ECWID-125708 The 'zzgl. Versand' line is displayed when free shipping is set for the product: replaced IgnoreNullable with AllowNullable for isShippingFree property

--- src/test/kotlin/com/ecwid/apiclient/v3/rule/nullablepropertyrules/FetchedProductRules.kt
+++ src/test/kotlin/com/ecwid/apiclient/v3/rule/nullablepropertyrules/FetchedProductRules.kt
@@ -42,7 +42,7 @@ val fetchedProductNullablePropertyRules: List<NullablePropertyRule<*, *>> = list
 	IgnoreNullable(FetchedProduct::isGiftCard),
 	IgnoreNullable(FetchedProduct::isSampleProduct),
 	IgnoreNullable(FetchedProduct::isShippingRequired),
-	IgnoreNullable(FetchedProduct::isShippingFree),
+	AllowNullable(FetchedProduct::isShippingFree),
 	IgnoreNullable(FetchedProduct::media),
 	IgnoreNullable(FetchedProduct::nameTranslated),
 	IgnoreNullable(FetchedProduct::nameYourPriceEnabled),
