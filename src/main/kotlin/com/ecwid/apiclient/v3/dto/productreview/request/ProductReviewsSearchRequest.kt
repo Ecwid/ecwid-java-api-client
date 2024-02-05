0b3b79605319b02ea136dd9121292eb65c209420
kitty <kitty@ecwid.com>
2024-02-05 14:36:24 +0400

    ECWID-133812 Fixed params for product reviews search

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/productreview/request/ProductReviewsSearchRequest.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/productreview/request/ProductReviewsSearchRequest.kt
@@ -6,18 +6,18 @@ import com.ecwid.apiclient.v3.dto.productreview.enums.ProductReviewSortOrder
 import com.ecwid.apiclient.v3.dto.productreview.enums.ProductReviewStatus
 import com.ecwid.apiclient.v3.impl.RequestInfo
 import com.ecwid.apiclient.v3.responsefields.ResponseFields
-import java.util.*
-import java.util.concurrent.TimeUnit
+import java.time.Instant
 
 data class ProductReviewsSearchRequest(
-	val reviewId: Long? = null,
-	val productId: Long? = null,
-	val orderId: Long? = null,
+	val reviewId: String? = null,
+	val productId: String? = null,
+	val orderId: String? = null,
 	val status: ProductReviewStatus? = null,
-	val createdFrom: Date? = null,
-	val createdTo: Date? = null,
-	val updatedFrom: Date? = null,
-	val updatedTo: Date? = null,
+	val rating: Int? = null,
+	val createdFrom: Instant? = null,
+	val createdTo: Instant? = null,
+	val updatedFrom: Instant? = null,
+	val updatedTo: Instant? = null,
 	val sortBy: ProductReviewSortOrder? = null,
 	val limit: Int = 100,
 	override val offset: Int = 0,
@@ -34,14 +34,15 @@ data class ProductReviewsSearchRequest(
 	private fun toParams(): Map<String, String> {
 		val request = this
 		return mutableMapOf<String, String>().apply {
-			request.reviewId?.let { put("reviewId", request.reviewId.toString()) }
-			request.productId?.let { put("productId", request.productId.toString()) }
-			request.orderId?.let { put("orderId", request.orderId.toString()) }
-			request.status?.let { put("status", request.status.toString()) }
-			request.createdFrom?.let { put("createdFrom", TimeUnit.MILLISECONDS.toSeconds(it.time).toString()) }
-			request.createdTo?.let { put("createdTo", TimeUnit.MILLISECONDS.toSeconds(it.time).toString()) }
-			request.updatedFrom?.let { put("updatedFrom", TimeUnit.MILLISECONDS.toSeconds(it.time).toString()) }
-			request.updatedTo?.let { put("updatedTo", TimeUnit.MILLISECONDS.toSeconds(it.time).toString()) }
+			request.reviewId?.let { put("reviewId", it) }
+			request.productId?.let { put("productId", it) }
+			request.orderId?.let { put("orderId", it) }
+			request.status?.let { put("status", it.toString()) }
+			request.rating?.let { put("rating", it.toString()) }
+			request.createdFrom?.let { put("createdFrom", it.toString()) }
+			request.createdTo?.let { put("createdTo", it.toString()) }
+			request.updatedFrom?.let { put("updatedFrom", it.toString()) }
+			request.updatedTo?.let { put("updatedTo", it.toString()) }
 			request.sortBy?.let { put("sortBy", it.name) }
 			put("offset", request.offset.toString())
 			put("limit", request.limit.toString())
