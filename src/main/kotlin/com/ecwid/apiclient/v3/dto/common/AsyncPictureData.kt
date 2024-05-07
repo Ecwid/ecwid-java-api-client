573cb1085bc70560ce7070d270d08164be4a041b
Zimu Zheng <zimu.zheng@lightspeedhq.com>
2024-05-07 12:12:53 +0200

    ECWID-116277 Implement alt-text support for all image API | Update AsyncPictureData dto

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/common/AsyncPictureData.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/common/AsyncPictureData.kt
@@ -3,5 +3,11 @@ package com.ecwid.apiclient.v3.dto.common
 data class AsyncPictureData(
 	val url: String = "",
 	val width: Int = 0,
-	val height: Int = 0
+	val height: Int = 0,
+	val alt: PictureAlt? = null
+)
+
+data class PictureAlt (
+	var main: String? = null,
+	var translated: Map<String, String>? = null
 )
