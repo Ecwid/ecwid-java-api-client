e5aaa6e919c9179a386489f63ecd5002d62ce252
Renat Ayzyatullen <renat.ayzyatullen@lightspeedhq.com>
2024-05-24 19:08:06 +0400

    ECWID-135200 Recurring subscription orders are created without Tax ID although it's presented in the initial order - changed customerFiscalCode default value to empty string (as in ApiOrder) in FetchedOrder and UpdatedOrder

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/order/request/UpdatedOrder.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/order/request/UpdatedOrder.kt
@@ -95,7 +95,7 @@ data class UpdatedOrder(
 	val orderExtraFields: List<OrderExtraFields>? = null,
 	val paymentReference: String? = null,
 	val loyalty: Loyalty? = null,
-	val customerFiscalCode: String? = null,
+	val customerFiscalCode: String? = "",
 
 ) : ApiUpdatedDTO {
 
