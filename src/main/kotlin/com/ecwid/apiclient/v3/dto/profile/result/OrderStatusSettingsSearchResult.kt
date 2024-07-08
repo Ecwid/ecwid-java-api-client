0e192226349763b6d4a86d037d31160ec92a4505
ArAkAru <arkadi97@mail.ru>
2024-07-08 10:54:04 +0400

    ECWID-142216 Ecwid Api changes: implement ApiResultDTO for generic parsing

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/OrderStatusSettingsSearchResult.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/OrderStatusSettingsSearchResult.kt
@@ -4,4 +4,5 @@ import com.ecwid.apiclient.v3.dto.common.ApiResultDTO
 
 data class OrderStatusSettingsSearchResult(
 	val items: List<FetchedOrderStatusSettings> = listOf(),
+	val limit: Int = 0,
 ) : ApiResultDTO
