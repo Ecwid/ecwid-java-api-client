b9e4d67d00b8a664076a852ba9cce8da76b99694
illarion.borts <illarion.borts@lightspeedhq.com>
2024-12-14 10:49:42 +0100

    ECWID-153945 - add field omniGiftCardsEnabled

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/FetchedStoreProfile.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/FetchedStoreProfile.kt
@@ -900,7 +900,8 @@ data class FetchedStoreProfile(
 
 	data class GiftCardSettings(
 		val products: List<GiftCardProducts>? = null,
-		val displayLocation: String? = null
+		val displayLocation: String? = null,
+		val omniGiftCardsEnabled: Boolean = false,
 	)
 
 	data class GiftCardProducts(
