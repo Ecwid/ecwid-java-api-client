0356f3195b9957f4c83ffd4b39a1507323dc4b3f
Roman Timchenko <roman.timchenko@lightspeedhq.com>
2025-01-21 17:15:17 +0400

    ECWID-155668 Add "updateShippingOption" method to ecwid-java-api-client - (feat) implement updateShippingOption method

---
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/result/UpdateShippingOptionResult.kt
@@ -0,0 +1,7 @@
+package com.ecwid.apiclient.v3.dto.profile.result
+
+import com.ecwid.apiclient.v3.dto.common.ApiResultDTO
+
+data class UpdateShippingOptionResult(
+	val updateCount: Int = 0
+) : ApiResultDTO
