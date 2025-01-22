1109053b47bad3e0ca5e465c805316a1803899bf
Roman Timchenko <roman.timchenko@lightspeedhq.com>
2025-01-22 10:09:30 +0400

    ECWID-155668 Add "updateShippingOption" method to ecwid-java-api-client - (refactor) fix detekt: get rid of magic numbers

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/enums/AvailabilityPeriodTimeUnit.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/enums/AvailabilityPeriodTimeUnit.kt
@@ -1,7 +1,10 @@
 package com.ecwid.apiclient.v3.dto.profile.enums
 
+private const val MINUTES_IN_DAY = 1_440 // 24 hours * 60 minutes
+private const val MINUTES_IN_MONTH = 43_200 // 30 * MINUTES_IN_DAY
+
 @Suppress("unused")
 enum class AvailabilityPeriodTimeUnit(val valueInMinutes: Int) {
-	DAYS(24 * 60),
-	MONTHS(30 * 24 * 60);
+	DAYS(MINUTES_IN_DAY),
+	MONTHS(MINUTES_IN_MONTH)
 }
--- src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/enums/AvailabilityPeriodType.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/enums/AvailabilityPeriodType.kt
@@ -1,8 +1,12 @@
 package com.ecwid.apiclient.v3.dto.profile.enums
 
+private const val DAYS_IN_WEEK = 7
+private const val SINGLE_UNIT = 1
+private const val MONTHS_IN_CENTURY = 1200
+
 @Suppress("unused")
 enum class AvailabilityPeriodType(val unitCount: Int, val unit: AvailabilityPeriodTimeUnit?) {
-	SEVEN_DAYS(7, AvailabilityPeriodTimeUnit.DAYS),
-	ONE_MONTH(1, AvailabilityPeriodTimeUnit.MONTHS),
-	UNLIMITED(1200, AvailabilityPeriodTimeUnit.MONTHS);
+	SEVEN_DAYS(DAYS_IN_WEEK, AvailabilityPeriodTimeUnit.DAYS),
+	ONE_MONTH(SINGLE_UNIT, AvailabilityPeriodTimeUnit.MONTHS),
+	UNLIMITED(MONTHS_IN_CENTURY, AvailabilityPeriodTimeUnit.MONTHS)
 }
