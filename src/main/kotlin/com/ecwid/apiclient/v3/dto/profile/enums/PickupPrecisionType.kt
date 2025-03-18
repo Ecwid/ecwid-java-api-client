555660c1bb93bec1c5b125d14eb2cc981eba01a8
Amith Gotamey <amith.gotamey@lightspeedhq.com>
2025-03-18 11:05:31 +0100

    ECWID-159524 Extract PickupPrecision into file

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/enums/PickupPrecisionType.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/profile/enums/PickupPrecisionType.kt
@@ -1,7 +1,7 @@
 package com.ecwid.apiclient.v3.dto.profile.enums
 
 @Suppress("unused")
-enum class ScheduledTimePrecisionType {
-	DATE,
-	DATE_AND_TIME_SLOT,
+enum class PickupPrecisionType {
+	DATE_ONLY,
+	DATE_AND_TIME,
 }
