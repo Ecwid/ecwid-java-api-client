22162c2e34db84a25f37b9e7257b03171a82d7cf
valkiriya <dinara.aleskarova@lightspeedhq.com>
2025-04-03 12:40:19 +0400

    Refactor ProductOptionChoiceDeserializer

--- src/main/kotlin/com/ecwid/apiclient/v3/jsontransformer/gson/typeadapters/ProductOptionChoiceDeserializer.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/jsontransformer/gson/typeadapters/ProductOptionChoiceDeserializer.kt
@@ -16,41 +16,22 @@ class FetchedProductOptionChoiceDeserializer : JsonDeserializer<FetchedProduct.P
 		typeOf: Type,
 		context: JsonDeserializationContext,
 	): FetchedProduct.ProductOptionChoice {
-		var text: String = ""
-		var textTranslated: LocalizedValueMap? = null
-		var priceModifier: Double = 0.0
-		var priceModifierType: PriceModifierType = PriceModifierType.ABSOLUTE
-		var type: ProductOptionChoiceType = ProductOptionChoiceType.SELECT
-		var hexCodes: List<String> = emptyList()
-		var imageId: String? = null
-
-		json.asJsonObject.keySet().forEach { key ->
-			val jsonElement = json.asJsonObject[key]
-			when (key) {
-				"text" -> text = jsonElement.asString
-				"textTranslated" -> textTranslated = context.deserialize(jsonElement, LocalizedValueMap::class.java)
-				"priceModifier" -> priceModifier = jsonElement.asDouble
-				"priceModifierType" -> priceModifierType = PriceModifierType.valueOf(jsonElement.asString.uppercase())
-				"type" -> type = ProductOptionChoiceType.valueOf(jsonElement.asString.uppercase())
-				"hexCodes" -> hexCodes = jsonElement.asJsonArray.map { it.asString }
-				"imageId" -> imageId = jsonElement.asString
-			}
-		}
-		return when (type) {
+		val template = buildProductOptionChoiceTemplate(json, context)
+		return when (template.type) {
 			ProductOptionChoiceType.SELECT -> FetchedProduct.ProductOptionChoice.SelectChoice(
-				text = text,
-				textTranslated = textTranslated,
-				priceModifier = priceModifier,
-				priceModifierType = priceModifierType,
+				text = template.text,
+				textTranslated = template.textTranslated,
+				priceModifier = template.priceModifier,
+				priceModifierType = template.priceModifierType,
 			)
 
 			ProductOptionChoiceType.SWATCH -> FetchedProduct.ProductOptionChoice.SwatchChoice(
-				text = text,
-				textTranslated = textTranslated,
-				priceModifier = priceModifier,
-				priceModifierType = priceModifierType,
-				hexCodes = hexCodes,
-				imageId = imageId,
+				text = template.text,
+				textTranslated = template.textTranslated,
+				priceModifier = template.priceModifier,
+				priceModifierType = template.priceModifierType,
+				hexCodes = template.hexCodes,
+				imageId = template.imageId,
 			)
 		}
 	}
@@ -62,43 +43,53 @@ class UpdatedProductOptionChoiceDeserializer : JsonDeserializer<UpdatedProduct.P
 		typeOf: Type,
 		context: JsonDeserializationContext,
 	): UpdatedProduct.ProductOptionChoice {
-		var text: String = ""
-		var textTranslated: LocalizedValueMap? = null
-		var priceModifier: Double = 0.0
-		var priceModifierType: PriceModifierType = PriceModifierType.ABSOLUTE
-		var type: ProductOptionChoiceType = ProductOptionChoiceType.SELECT
-		var hexCodes: List<String> = emptyList()
-		var imageId: String? = null
-
-		json.asJsonObject.keySet().forEach { key ->
-			val jsonElement = json.asJsonObject[key]
-			when (key) {
-				"text" -> text = jsonElement.asString
-				"textTranslated" -> textTranslated = context.deserialize(jsonElement, LocalizedValueMap::class.java)
-				"priceModifier" -> priceModifier = jsonElement.asDouble
-				"priceModifierType" -> priceModifierType = PriceModifierType.valueOf(jsonElement.asString.uppercase())
-				"type" -> type = ProductOptionChoiceType.valueOf(jsonElement.asString.uppercase())
-				"hexCodes" -> hexCodes = jsonElement.asJsonArray.map { it.asString }
-				"imageId" -> imageId = jsonElement.asString
-			}
-		}
-		return when (type) {
+		val template = buildProductOptionChoiceTemplate(json, context)
+		return when (template.type) {
 			ProductOptionChoiceType.SELECT -> UpdatedProduct.ProductOptionChoice.SelectChoice(
-				text = text,
-				textTranslated = textTranslated,
-				priceModifier = priceModifier,
-				priceModifierType = priceModifierType,
+				text = template.text,
+				textTranslated = template.textTranslated,
+				priceModifier = template.priceModifier,
+				priceModifierType = template.priceModifierType,
 			)
 
 			ProductOptionChoiceType.SWATCH -> UpdatedProduct.ProductOptionChoice.SwatchChoice(
-				text = text,
-				textTranslated = textTranslated,
-				priceModifier = priceModifier,
-				priceModifierType = priceModifierType,
-				hexCodes = hexCodes,
-				imageId = imageId,
+				text = template.text,
+				textTranslated = template.textTranslated,
+				priceModifier = template.priceModifier,
+				priceModifierType = template.priceModifierType,
+				hexCodes = template.hexCodes,
+				imageId = template.imageId,
 			)
 		}
 	}
+}
 
+data class ProductOptionChoiceTemplate(
+	var text: String = "",
+	var textTranslated: LocalizedValueMap? = null,
+	var priceModifier: Double = 0.0,
+	var priceModifierType: PriceModifierType = PriceModifierType.ABSOLUTE,
+	var type: ProductOptionChoiceType = ProductOptionChoiceType.SELECT,
+	var hexCodes: List<String> = emptyList(),
+	var imageId: String? = null,
+)
+
+private fun buildProductOptionChoiceTemplate(
+	json: JsonElement,
+	context: JsonDeserializationContext,
+): ProductOptionChoiceTemplate {
+	val template = ProductOptionChoiceTemplate()
+	json.asJsonObject.keySet().forEach { key ->
+		val jsonElement = json.asJsonObject[key]
+		when (key) {
+			"text" -> template.text = jsonElement.asString
+			"textTranslated" -> template.textTranslated = context.deserialize(jsonElement, LocalizedValueMap::class.java)
+			"priceModifier" -> template.priceModifier = jsonElement.asDouble
+			"priceModifierType" -> template.priceModifierType = PriceModifierType.valueOf(jsonElement.asString.uppercase())
+			"type" -> template.type = ProductOptionChoiceType.valueOf(jsonElement.asString.uppercase())
+			"hexCodes" -> template.hexCodes = jsonElement.asJsonArray.map { it.asString }
+			"imageId" -> template.imageId = jsonElement.asString
+		}
+	}
+	return template
 }
