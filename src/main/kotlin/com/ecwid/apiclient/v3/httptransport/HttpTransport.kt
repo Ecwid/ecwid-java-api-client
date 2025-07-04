f8622e5cbded9116e39a78341b05942a0749cd32
Marat Amirov <marat.amirov@lightspeedhq.com>
2025-07-04 15:02:25 +0400

    ECWID-165744 Update Apache httpclient to httpclient5 5.5, fix detekt

--- src/main/kotlin/com/ecwid/apiclient/v3/httptransport/HttpTransport.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/httptransport/HttpTransport.kt
@@ -2,6 +2,6 @@ package com.ecwid.apiclient.v3.httptransport
 
 import java.io.Closeable
 
-interface HttpTransport: Closeable {
+interface HttpTransport : Closeable {
 	fun makeHttpRequest(httpRequest: HttpRequest): HttpResponse
 }
