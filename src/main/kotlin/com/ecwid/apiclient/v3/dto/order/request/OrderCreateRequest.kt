3a8f46b67f56ace130d612d64f51e3f5b226ed8d
Oleg Shchelykalnov <oleg.shchelykalnov@lightspeedhq.com>
2025-11-04 17:08:00 +0400

    ECWID-XXXXXX OE2: Add createTaxInvoiceRequired parameter to create order request

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/order/request/OrderCreateRequest.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/order/request/OrderCreateRequest.kt
@@ -5,7 +5,8 @@ import com.ecwid.apiclient.v3.httptransport.HttpBody
 import com.ecwid.apiclient.v3.impl.RequestInfo
 
 data class OrderCreateRequest(
-	val newOrder: UpdatedOrder = UpdatedOrder()
+	val newOrder: UpdatedOrder = UpdatedOrder(),
+	val createTaxInvoiceRequired: Boolean = false,
 ) : ApiRequest {
 	override fun toRequestInfo() = RequestInfo.createPostRequest(
 		pathSegments = listOf(
@@ -13,6 +14,11 @@ data class OrderCreateRequest(
 		),
 		httpBody = HttpBody.JsonBody(
 			obj = newOrder
-		)
+		),
+		params = if (createTaxInvoiceRequired) {
+			mapOf("createTaxInvoiceRequired" to "true")
+		} else {
+			emptyMap()
+		}
 	)
 }
