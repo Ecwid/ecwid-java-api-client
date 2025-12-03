55f9764b4392798590ce7e5e3174824a8209e0a9
Marat Amirov <marat.amirov@lightspeedhq.com>
2025-12-03 13:38:48 +0400

    ECWID-175387 X+E: Stream additional product identifiers. Export, add AttributeValueAlias for external product codes

--- src/main/kotlin/com/ecwid/apiclient/v3/dto/product/enums/AttributeValueAlias.kt
+++ src/main/kotlin/com/ecwid/apiclient/v3/dto/product/enums/AttributeValueAlias.kt
@@ -7,6 +7,12 @@ enum class AttributeValueAlias {
 	BRAND,
 	PRICE_PER_UNIT,
 	UNITS_IN_PRODUCT,
+	EXTERNAL_EAN,
+	EXTERNAL_ISBN,
+	EXTERNAL_ITF,
+	EXTERNAL_JAN,
+	EXTERNAL_CUSTOM,
+	EXTERNAL_UPC,
 }
 
 fun AttributeType.toAttributeValueAlias(): AttributeValueAlias? {
@@ -15,6 +21,12 @@ fun AttributeType.toAttributeValueAlias(): AttributeValueAlias? {
 		AttributeType.BRAND -> AttributeValueAlias.BRAND
 		AttributeType.PRICE_PER_UNIT -> AttributeValueAlias.PRICE_PER_UNIT
 		AttributeType.UNITS_IN_PRODUCT -> AttributeValueAlias.UNITS_IN_PRODUCT
+		AttributeType.EXTERNAL_EAN -> AttributeValueAlias.EXTERNAL_EAN
+		AttributeType.EXTERNAL_ISBN -> AttributeValueAlias.EXTERNAL_ISBN
+		AttributeType.EXTERNAL_ITF -> AttributeValueAlias.EXTERNAL_ITF
+		AttributeType.EXTERNAL_JAN -> AttributeValueAlias.EXTERNAL_JAN
+		AttributeType.EXTERNAL_CUSTOM -> AttributeValueAlias.EXTERNAL_CUSTOM
+		AttributeType.EXTERNAL_UPC -> AttributeValueAlias.EXTERNAL_UPC
 		else -> null
 	}
 }
